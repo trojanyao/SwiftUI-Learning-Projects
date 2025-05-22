5750785bdac8bdab0c5f1b9a714df8f1d56a91cb
TROJAN <ytj1996@gmail.com>
2025-05-22 21:02:39 +0800

    feat: Chapter 5 - Lists and text fields

---
+++ Chapter 5 - Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by 姚陶钧 on 2025/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
