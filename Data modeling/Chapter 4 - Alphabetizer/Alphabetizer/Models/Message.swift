a3ee1ec162320da3d46ad8bd260913fffbb28ed1
TROJAN <ytj1996@gmail.com>
2025-07-10 21:25:18 +0800

    feat(Data modeling): Chapter 4 - Observation and shareable data models

---
+++ Data modeling/Chapter 4 - Alphabetizer/Alphabetizer/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Alphabetizer
+//
+//  Created by TROJAN on 2025/7/9.
+//
+
+import Foundation
+
+enum Message: String {
+    case instructions = "Place the tiles in alphabetical order"
+    case tryAgain = "Almost 😅"
+    case youWin = "You win! 🏆"
+}
---
+++ Data modeling/Chapter 4 - Alphabetizer/Alphabetizer/Views/MessageView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct MessageView: View {
+    @Environment(Alphabetizer.self) private var alphabetizer
+
+    var body: some View {
+        Text(alphabetizer.message.rawValue)
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    let alphabetizer = Alphabetizer()
+    alphabetizer.message = .youWin
+    
+    return MessageView()
+        .environment(alphabetizer)
+}
